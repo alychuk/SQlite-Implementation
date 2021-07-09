6778b53525e5eec9cc809d80826a09442c2b892d
alychuk <22718179+alychuk@users.noreply.github.com>
2021-07-09 03:45:29 -0700

    Add files via upload

---
+++ pa3/driver.py
@@ -0,0 +1,22 @@
+#CS457 UNR
+#Author: Adam Lychuk
+import sys
+import re
+from parser import Parser
+
+def main():
+    parser = Parser()
+    # Since argv is 1 expect standard input
+    if len(sys.argv) == 1:
+        print("Enter command: ")
+        lines = ""
+        for line in sys.stdin:
+            lines = lines + line.rstrip() + "\n"
+        parser.run_commands(lines)
+        print("Enter command: ")
+    # expect file name in command line argument
+    else:
+        parser.run_commands(" ".join(sys.argv))
+
+if __name__ == "__main__":
+    main()
